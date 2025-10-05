050c86743248d30e0de8ee63adf0055507cd9175
toufik0661 <toufiqurrahman0661@gmail.com>
2025-10-05 21:54:46 +0600

    How to find min

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 function maximum(){
   let array = [4, 5, 6 ,13, 45, 56, 78]
-  let max= array[0]
-  for(let i=0; i<array.length; i++){
-      if(array[i]>max){
-        max= array[i]
+  let max = array[0];
+  for(let i = 0; i < array.length; i++){
+      if(array[i] > max){
+        max = array[i]
       }
      
   }
    console.log(`The lergest element in array is ${max}`)
 }
 maximum();
+
+
+function minimum (){
+  let array = [4, 5, 6, 13, 45, 56, 78]
+  let min = array[0];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] < min){
+      min = array[i]
+    }
+    
+  }
+  console.log(`The smallest element in array is ${min}`)
+
+}
+minimum()
