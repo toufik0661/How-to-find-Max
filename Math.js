65d85ad817312a50e58a88c6c928dfffc95b1429
toufik0661 <toufiqurrahman0661@gmail.com>
2025-10-05 22:01:21 +0600

    math is so good

---
+++ Math.js
@@ -0,0 +1,8 @@
+let array = [4, 6, 7, 8, 9, 20, 53, 56]
+
+let max = Math.max(...array);
+let min = Math.min(...array);
+
+console.log(`The largest element in array is ${max}`);
+console.log(`The smallest elenent in array is ${min}`);
+
